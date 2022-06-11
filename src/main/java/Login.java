d7eaa08cb27c1fb0b5a8bc0c55dbee658296b61e
memreyagci <emre@meyagci.com>
2022-06-11 20:52:22 +0800

    Login class is created

---
+++ src/main/java/Login.java
@@ -0,0 +1,52 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import pages.LoginPage;
+
+public class Login {
+    private final String cydeoUrl = "https://cydeo.com";
+    private LoginPage loginPage;
+
+    public Login() {
+        loginPage = new LoginPage();
+    }
+
+    public void loginToCydeo(WebDriver driver, String email, String password) {
+        driver.get(cydeoUrl);
+        driver.findElement(By.linkText("Log In")).click();
+
+        loginPage.inputEmail.sendKeys(email);
+        loginPage.signInButton.click();
+        loginPage.inputPassword.sendKeys(password);
+
+        if(isSignInButtonDisabled()) {
+            throw new Error("Neither email nor password field can be empty. Exiting..");
+        } else {
+            loginPage.signInButton.click();
+
+            if(!isLoginSuccessful()) {
+                throw new Error("Either email or password is wrong. Exiting..");
+            }
+        }
+    }
+
+    private boolean isSignInButtonDisabled() {
+        try {
+            if(loginPage.signInButtonDisabled.isDisplayed()) {
+                return true;
+            } else return false;
+        } catch(NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isLoginSuccessful() {
+        try {
+            if(!loginPage.wrongEmailOrPasswordErrorMessage.isDisplayed()) {
+                return true;
+            } else return false;
+        } catch(NoSuchElementException e) {
+            return true;
+        }
+    }
+}
