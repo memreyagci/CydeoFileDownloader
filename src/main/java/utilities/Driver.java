38eb6484f9f36deec34d10dad14c2a893034d7e0
memreyagci <emre@meyagci.com>
2022-06-12 09:24:38 +0800

    setChromeOptions method created.
    This method allows us to set a download path for Chrome browser

--- src/main/java/utilities/Driver.java
+++ src/main/java/utilities/Driver.java
@@ -3,9 +3,13 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -15,6 +19,7 @@ public class Driver {
     }
 
     private static WebDriver driver;
+    private static ChromeOptions options;
 
     public static WebDriver getDriver() {
 
@@ -29,7 +34,7 @@ public class Driver {
             switch (browserType) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    driver = new ChromeDriver(options);
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
@@ -59,5 +64,16 @@ public class Driver {
             driver.quit(); // this line will terminate the existing session. value will not even be null
             driver = null;
         }
+    }
+
+    public static ChromeOptions setChromeOptions(String path) {
+        Map<String,Object> preferences= new HashMap<>();
+        preferences.put("profile.default_content_settings.popups", 0);
+        preferences.put("download.default_directory", path);
+
+        options = new ChromeOptions();
+        options.setExperimentalOption("prefs", preferences);
+
+        return options;
     }
 }
